b3f9980663774b89a3c3f19b8ea68ef3c5690c97
shikharmanandhar <shikharaa26@gmail.com>
2022-01-12 11:35:39 +0545

    added bcrypt js

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 //const mongoose = require("mongoose");
-// import bcrypt from "bcryptjs";
+import bcrypt from "bcryptjs";
 
 const userSchema = mongoose.Schema(
   {
