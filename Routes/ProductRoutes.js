c8ddbd4a6a278fea36c887d39e05fb5808fb8300
shikharmanandhar <shikharaa26@gmail.com>
2022-02-06 18:21:46 +0545

    Editing product route

--- Routes/ProductRoutes.js
+++ Routes/ProductRoutes.js
@@ -39,5 +39,19 @@ productRoute.get(
   })
 );
 
+// GET SINGLE PRODUCT
+productRoute.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404);
+      throw new Error("Product not Found");
+    }
+  })
+);
+
 
 export default productRoute;
