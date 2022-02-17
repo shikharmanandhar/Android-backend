aa35b56721ec807a7b30ca0a1a364939c5ce35e1
shikharmanandhar <shikharaa26@gmail.com>
2022-02-17 18:24:33 +0545

    Editing order route

--- Routes/orderRoutes.js
+++ Routes/orderRoutes.js
@@ -42,5 +42,46 @@ orderRouter.post(
   })
 );
 
+// ADMIN GET ALL ORDERS
+orderRouter.get(
+  "/all",
+  protect,
+  admin,
+  asyncHandler(async (req, res) => {
+    const orders = await Order.find({})
+      .sort({ _id: -1 })
+      .populate("user", "id name email");
+    res.json(orders);
+  })
+);
+// USER LOGIN ORDERS
+orderRouter.get(
+  "/",
+  protect,
+  asyncHandler(async (req, res) => {
+    const order = await Order.find({ user: req.user._id }).sort({ _id: -1 });
+    res.json(order);
+  })
+);
+
+// GET ORDER BY ID
+orderRouter.get(
+  "/:id",
+  protect,
+  asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id).populate(
+      "user",
+      "name email"
+    );
+
+    if (order) {
+      res.json(order);
+    } else {
+      res.status(404);
+      throw new Error("Order Not Found");
+    }
+  })
+);
+
 
 export default orderRouter;
